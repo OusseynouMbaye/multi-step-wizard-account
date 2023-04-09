1649e64fd43cc80714a8023a3e08679e699ee120
Ouzinem <ouzinem@hotmail.com>
2023-04-09 06:20:01 -0400

    component multi page wizard

---
+++ src/components/Wizard.vue
@@ -0,0 +1,11 @@
+<template>
+  <WizardStep2 />
+  <WizardStep1 />
+  <WizardSummary />
+</template>
+
+<script lang="ts" setup>
+import WizardStep1 from "./WizardStep1.vue";
+import WizardStep2 from "./WizardStep2.vue";
+import WizardSummary from "./WizardSummary.vue";
+</script>
